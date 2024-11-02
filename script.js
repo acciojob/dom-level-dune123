5ddb9d076b0070cdab5d7838e82cf2a6d714f33a
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-02 12:10:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function getDOMlevel(elementId){
+	let element=document.getElementById(elementId);
+
+	let level=0;
+	while(element){
+		level++;
+		element = element.parentElement;
+	}
+	return level;
+}
+
+console.log(getDOMlevel("level"))
